7c0af418150d6a53c9b98bae765e522864b2c32d
thduy190426 <duyhoangtran2006@gmail.com>
2026-07-03 13:04:57 +0700

    update-ui

--- src/components/views/SettingsPage.tsx
+++ src/components/views/SettingsPage.tsx
@@ -113,8 +113,11 @@ function formatDateTime(value: string | null) {
   }
 
   return new Intl.DateTimeFormat('vi-VN', {
-    dateStyle: 'short',
-    timeStyle: 'short',
+    hour: '2-digit',
+    minute: '2-digit',
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
   }).format(date)
 }
 
